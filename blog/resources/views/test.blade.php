2be53560418f4d07f6369072091404a6c0ddd047
xuantoan0406 <doxuantoan00@gmail.com>
2020-05-06 11:36:28 +0700

    hel

---
+++ blog/resources/views/test.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<example-component></example-component>
+<script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
