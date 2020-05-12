7a3eb28ce50ed5755738f58c2351be06b20557dc
xuantoan0406 <doxuantoan00@gmail.com>
2020-05-12 11:56:39 +0700

    new

--- blog/public/js/app.js
+++ blog/public/js/app.js
@@ -49795,7 +49795,7 @@ window.Vue = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.common.
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', __webpack_require__(/*! ./components/ExampleComponent.vue */ "./resources/js/components/ExampleComponent.vue")["default"]);
+Vue.component("example", __webpack_require__(/*! ./components/ExampleComponent.vue */ "./resources/js/components/ExampleComponent.vue")["default"]);
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
@@ -49803,7 +49803,7 @@ Vue.component('example-component', __webpack_require__(/*! ./components/ExampleC
  */
 
 var app = new Vue({
-  el: '#app'
+  el: "#app"
 });
 
 /***/ }),
@@ -49940,8 +49940,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/xuantoan/Desktop/blog/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/xuantoan/Desktop/blog/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/xuantoan/Desktop/blog/blog/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/xuantoan/Desktop/blog/blog/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -23,3 +23,6 @@ Route::get('test', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('vue', function () {
+    return view('vue');
+});
